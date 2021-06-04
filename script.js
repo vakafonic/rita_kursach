48e9095644594477869b6bef08d127bd9fce8a5d
Ivan <vakafonic@gmail.com>
2021-06-04 16:14:14 +0300

    Start

---
+++ script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+ 
+//     var $menu = $("#menu");
+
+//     $(window).scroll(function(){
+//         if ( $(this).scrollTop() > 100 && $menu.hasClass("menus") ){
+//             $menu.removeClass("menus").addClass("fixed");
+//         } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+//             $menu.removeClass("fixed").addClass("menus");
+//         }
+//     });//scroll
+// });
